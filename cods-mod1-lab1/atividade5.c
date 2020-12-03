960fda739562395ac17ddbc63297f08e3193ab69
Joao Seda <joao_ps@dcc.ufrj.br>
2020-12-03 10:09:44 -0300

    Atividade 5

---
+++ cods-mod1-lab1/atividade5.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+
+#define N 10
+
+typedef struct {
+    int *priElem;
+    int NElem, idThread;
+} t_Args;
+
+void *IncVet(void *arg) {
+    t_Args *args = (t_Args *) arg;
+    int i;
+
+    for( i = 0; i < args->NElem; i++ ) {
+        (args->priElem[i])++;
+    }
+    printf("---Thread %d finalizada---\n\n", args->idThread);
+    free(arg);
+
+
+    pthread_exit(NULL);
+}
+
+int main() {
+    pthread_t tid_sistema[2];
+    int vetor[N], i, metadeVetor;
+    t_Args *arg;
+
+    printf("Valores iniciais do vetor:\n");
+
+    for( i = 0; i < N; i++ ) {
+        vetor[i] = 0;
+        printf("%d\n", vetor[i]);
+    }
+
+    printf("\n");
+
+    metadeVetor = N/2; // Divide o vetor em duas metades, cada thread trabalha em uma metade
+
+    arg = malloc(sizeof(t_Args));
+    if(arg == NULL) exit(-1);
+
+    arg->idThread = 1;
+    arg->priElem = vetor;
+    arg->NElem = metadeVetor;
+
+    printf("---Inicia a thread 1---\n\n");
+    if (pthread_create(&tid_sistema[0], NULL, IncVet, (void*) arg)) exit(-1);
+
+    arg = malloc(sizeof(t_Args));
+    if(arg == NULL) exit(-1);
+
+    arg->idThread = 2;
+    arg->priElem = &vetor[metadeVetor];
+    arg->NElem = N - metadeVetor;
+
+    printf("---Inicia a thread 2---\n\n");
+    if (pthread_create(&tid_sistema[1], NULL, IncVet, (void*) arg)) exit(-1);
+
+    if (pthread_join(tid_sistema[0], NULL)) exit(-1);
+    if (pthread_join(tid_sistema[1], NULL)) exit(-1);
+
+    printf("---Todas as threads terminadas---\n\n");
+
+    printf("Valores finais do vetor:\n");
+
+    for( i = 0; i < N; i++ ) {
+        printf("%d\n", vetor[i]);
+    }
+
+    return 1;
+}
