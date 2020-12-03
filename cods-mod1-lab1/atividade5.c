6f9004c21de3c0ae07e29622147d6dbb38b38fcd
Joao Seda <joao_ps@dcc.ufrj.br>
2020-12-03 16:45:15 -0300

    Lab1

--- cods-mod1-lab1/atividade5.c
+++ cods-mod1-lab1/atividade5.c
@@ -4,6 +4,9 @@
 
 #define N 10
 
+// A thread recebe um ponteiro para o primeiro elemento do vetor que ela incrementará, 
+// um inteiro indicando quantos elementos ele deve incrementar e um inteiro que identifica
+// qual a thread executada
 typedef struct {
     int *priElem;
     int NElem, idThread;
@@ -24,48 +27,50 @@ void *IncVet(void *arg) {
 }
 
 int main() {
-    pthread_t tid_sistema[2];
-    int vetor[N], i, metadeVetor;
-    t_Args *arg;
+    pthread_t tid_sistema[2];   // Identificadores para as duas threads
+    int vetor[N];               // Vetor que será incrementado
+    int i, metadeVetor;         // Variáveis auxiliares
+    t_Args *arg;                // Argumentos para as threads
 
+    // Printa os valores iniciais do vetor
     printf("Valores iniciais do vetor:\n");
-
     for( i = 0; i < N; i++ ) {
-        vetor[i] = 0;
         printf("%d\n", vetor[i]);
     }
-
     printf("\n");
 
     metadeVetor = N/2; // Divide o vetor em duas metades, cada thread trabalha em uma metade
 
+    // Inicializa e atribui os argumentos da primeira thread
     arg = malloc(sizeof(t_Args));
     if(arg == NULL) exit(-1);
-
     arg->idThread = 1;
     arg->priElem = vetor;
     arg->NElem = metadeVetor;
 
+    // Inicia a primeira thread
     printf("---Inicia a thread 1---\n\n");
     if (pthread_create(&tid_sistema[0], NULL, IncVet, (void*) arg)) exit(-1);
 
+    // Inicializa e atribui os argumentos da segunda thread
     arg = malloc(sizeof(t_Args));
     if(arg == NULL) exit(-1);
-
     arg->idThread = 2;
     arg->priElem = &vetor[metadeVetor];
     arg->NElem = N - metadeVetor;
 
+    // Inicia a segunda thread
     printf("---Inicia a thread 2---\n\n");
     if (pthread_create(&tid_sistema[1], NULL, IncVet, (void*) arg)) exit(-1);
 
+    // Espera as duas threads finalizarem
     if (pthread_join(tid_sistema[0], NULL)) exit(-1);
     if (pthread_join(tid_sistema[1], NULL)) exit(-1);
 
     printf("---Todas as threads terminadas---\n\n");
 
+    // Printa os valores finais do vetor
     printf("Valores finais do vetor:\n");
-
     for( i = 0; i < N; i++ ) {
         printf("%d\n", vetor[i]);
     }
