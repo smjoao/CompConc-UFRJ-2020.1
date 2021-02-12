3a90701806ce3261cf46e67878b4e2711ed1c921
Joao Seda <joao_ps@dcc.ufrj.br>
2021-02-11 23:34:35 -0300

    Trab 2

--- Trab2/MonitorQuickSort.java
+++ Trab2/MonitorQuickSort.java
@@ -2,12 +2,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class MonitorQuickSort {
-    int list_size;
-    int max_cap;
-    int ordenados;
-    public boolean logs;
-    Queue<Args> buffer;
+    int list_size; // Tamanho do vetor que será ordenado
+    int max_cap; // Capacidade máxima do buffer
+    int ordenados; // Quantidade de posições ordenadas
+    public boolean logs; // Define se imprime os logs das threads ou não
+    Queue<Args> buffer; // Buffer do monitor
 
+    // Construtor
     public MonitorQuickSort(Args init_args, int max_cap, int list_size, boolean logs) {
         this.buffer = new LinkedList<>();
         this.buffer.add(init_args);
@@ -17,21 +18,30 @@ class MonitorQuickSort {
         this.logs = logs;
     }
 
+    // Método para inserir itens no buffer
     public synchronized void InserirBuffer(int id, Args arg) {
         try {
+            // Se o buffer estiver cheio, bloqueia a thread
             while(buffer.size() == max_cap) {
                 if(logs) System.out.println("Thread[" + id + "] bloqueada ao inserir!");
                 wait();
                 if(logs) System.out.println("Thread[" + id + "] desbloqueada para inserir!");
             }
         } catch (InterruptedException e) { }
+
+        // Adiciona o objeto ao buffer
         this.buffer.add(arg);
+
         if(logs) System.out.println("Thread[" + id + "] inseriu [" + arg.inicio + ", " + arg.fim + "] no buffer.");
-        notifyAll();
+        
+        // Desbloqueia uma thrad para o caso do buffer estar vazio anteriormente 
+        notify();
     }
 
+    // Método para remover itens do buffer
     public synchronized Args RemoverBuffer(int id) {
         try {
+            // Se o buffer estiver vazio e a ordenação ainda não tiver terminado, bloqueia a thread
             while(buffer.size() == 0 && ordenados != list_size) {
                 if(logs) System.out.println("Thread[" + id + "] bloqueada ao remover!");
                 wait();
@@ -39,20 +49,34 @@ class MonitorQuickSort {
             }
         } catch (InterruptedException e) { }
 
+        // Se a ordenação já tiver terminado, não retorna nada
+        // Essa verificação serve para o caso de algumas threads
+        // ainda estiverem bloqueadas após o término da ordenação
         if(ordenados == list_size) return null;
 
+        // Remove um item do buffer
         Args arg = this.buffer.remove();
-        notifyAll();
+
+        // Desbloqueia uma outra thread para o caso do buffer estar cheio anteriormente
+        notify();
+        
         if(logs) System.out.println("Thread[" + id + "] removeu [" + arg.inicio + ", " + arg.fim + "] do buffer.");
+        
+        // Retorna o objeto removido do buffer
         return arg;
     }
 
     public synchronized void AddOrdenados() {
+        // Incrementa a variável ordenadas
         this.ordenados++;
+
+        // Se todas as posições estiverem ordenadas, notifica todas
+        // as threads para que elas possam finalizar a execução
         if(this.ordenados == list_size) notifyAll();
     }
 
     public synchronized int GetOrdenados() {
+        // Retorn ordenados
         return this.ordenados;
     }
 }
--- Trab2/QuickSortConc.java
+++ Trab2/QuickSortConc.java
@@ -3,6 +3,7 @@ import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+// Objeto que será inserido no buffer
 class Args {
     public int inicio, fim;
 
@@ -10,43 +11,61 @@ class Args {
         this.inicio = inicio;
         this.fim = fim;
     }
-
 }
 
+// Classe das Threads
 class QuickSortThread extends Thread {
-    int id;
-    ArrayList<Integer> list;
-    MonitorQuickSort monitor;
+    int id; // Identificador das threads
+    ArrayList<Integer> list; // Vetor que será ordenado
+    MonitorQuickSort monitor; // Monitor das threads
 
+    // Construtor
     public QuickSortThread(int id, ArrayList<Integer> list, MonitorQuickSort monitor) {
         this.id = id;
         this.list = list;
         this.monitor = monitor;
     }
 
+    // Tarefa executada pelas threads
     public void run() {
+        // Tenta tirar um objeto do buffer
         Args arg = monitor.RemoverBuffer(this.id);
+
+        // Enquanto o vetor ainda não estiver totalmente ordenado
         while(monitor.GetOrdenados() < list.size()) {
 
-            int pivot = list.get(arg.fim);
-            int i = arg.inicio;
+            // Aplica o Quick Sort na seção do vetor representado pelo arg
 
+            int pivot = list.get(arg.fim); // Escolhe o pivô como o último item da seção do vetor
+            int i = arg.inicio; // Inicia o índice dos elementos menores que o pivô
+
+            // Passa por todos os itens da seção
             for(int j = arg.inicio; j < arg.fim; j++) {
+
+                // Se o item for menor que o pivô troca com o item no índice i,
+                // que representa o índice pros elementos menores que o pivô, e incrementa i
                 if(list.get(j) < pivot) {
                     Collections.swap(list, i, j);
                     i++;
                 }
             }
+
+            // No final troca o pivô com o item no índice i, 
+            // já que todos os elementos antes desse índice são menores que o pivô
             Collections.swap(list, i, arg.fim);
 
+            // Incrementa ordenados
             monitor.AddOrdenados();
 
+            // Inicia os objetos que serão inseridos no buffer
             Args arg1 = new Args(arg.inicio, i - 1);
             Args arg2 = new Args(i + 1, arg.fim);
             
+            // Insere os objetos no buffer se eles forem válidos (inicio <= fim)
             if(arg1.inicio <= arg1.fim) monitor.InserirBuffer(this.id, arg1);
             if(arg2.inicio <= arg2.fim) monitor.InserirBuffer(this.id, arg2);
             
+            // Se a ordenação ainda não tiver terminado, remove mais um objeto do buffer
             if(monitor.GetOrdenados() < list.size()) arg = monitor.RemoverBuffer(this.id);
         }
         if(monitor.logs) System.out.println("Thread[" + this.id + "] terminada!");
@@ -55,7 +74,7 @@ class QuickSortThread extends Thread {
 
 class QuickSortConc {
     public static void main(String[] args) {
-        boolean logs = false;
+        boolean logs = false; // Define se exibe logs das threads ou não
         
         if(args.length < 2) {
             System.out.println("Uso correto do programa:");
@@ -71,6 +90,7 @@ class QuickSortConc {
             System.exit(1);
         }
 
+        // Inicia o vetor e preenche ele com o arquivo in.txt
         ArrayList<Integer> list = new ArrayList<>();
         try {
             BufferedReader br = new BufferedReader(new FileReader("in.txt"));
@@ -81,33 +101,44 @@ class QuickSortConc {
             }
         } catch(Exception e) { System.out.println(e.getMessage()); System.exit(-1); }
 
-        System.out.println("Lista original: " + list);
+        // Imprime o vetor original
+        System.out.println("Vetor original: " + list);
 
+        // Inicia o buffer com o primeiro elemento representando o vetor inteiro
         Args init_args = new Args(0, list.size() - 1);
 
+        // Inicia o monitor e o vetor de threads, define capacidade 
+        // máx. do buffer como maior valor possível para inteiro
         MonitorQuickSort monitor = new MonitorQuickSort(init_args, Integer.MAX_VALUE, list.size(), logs);
         QuickSortThread[] threads = new QuickSortThread[num_threads];
 
+        // Começa a contar o tempo de execução
         long tempo_ini = System.currentTimeMillis();
 
+        // Cria e inicia as threads
         for(int i = 0; i < num_threads; i++) {
             threads[i] = new QuickSortThread(i, list, monitor);
             threads[i].start();
         }
 
+        // Espera todas as threads terminarem
         for(int i = 0; i < num_threads; i++) {
             try { threads[i].join(); }
             catch (InterruptedException e) { }
         }
 
+        // Termina a contagem de tempo
         long tempo_fin = System.currentTimeMillis();
 
-        System.out.println("Lista ordenada: " + list);
-
+        // Imprime o vetor ordenado
+        System.out.println("Vetor ordenado: " + list);
+        
+        // Código para a verificação da corretude
 //        for(int i = 0; i < list.size(); i++) {
 //            System.out.println(list.get(i));
 //        }
 
+        // Imprime o tempo de execução
         System.out.println("Tempo de execução: " + (tempo_fin - tempo_ini) + "ms");
     }
 }
